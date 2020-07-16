400d185ec827fa74f98597350c0de14f4b4c18ae
Camden Bruce <camden.o.b@gmail.com>
2020-07-17 08:20:08 +1200

    Finish off with script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # postmarketos-j4primelte-classic
 classic version using android kernel instead of mailine or halium.
+
+clone this to your home folder, make sure you have pmaports cloned in cache_git.
---
+++ install-to-pmaports-folder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -avr $HOME/postmarketos-j4primelte-classic/linux-samsung-j4primelte $HOME/.local/var/pmbootstrap/cache_git/pmaports/device/testing/ 
+cp -avr $HOME/postmarketos-j4primelte-classic/device-samsung-j4primelte $HOME/.local/var/pmbootstrap/cache_git/pmaports/device/testing/ 
+ehco "done"
+
