cb79d9451037b1e3560829b986ae2fc84190e672
Camden Bruce <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2020-07-17 11:29:30 +1200

    Update install-to-pmaports-folder.sh

--- install-to-pmaports-folder.sh
+++ install-to-pmaports-folder.sh
@@ -2,5 +2,5 @@
 
 cp -avr $HOME/postmarketos-j4primelte-classic/linux-samsung-j4primelte $HOME/.local/var/pmbootstrap/cache_git/pmaports/device/testing/ 
 cp -avr $HOME/postmarketos-j4primelte-classic/device-samsung-j4primelte $HOME/.local/var/pmbootstrap/cache_git/pmaports/device/testing/ 
-ehco "done"
+echo "done"
 
